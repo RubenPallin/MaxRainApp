d330558091097f9e3ec40068c73cc6c8f399bf06
Rubén Pallín González <RGonzalez@QUEIRUA.LOCAL>
2024-04-17 16:28:15 +0200

    Comenzando el proyecto

--- MyPFC/app/build.gradle.kts
+++ MyPFC/app/build.gradle.kts
@@ -34,7 +34,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation("com.android.volley:1.2.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
