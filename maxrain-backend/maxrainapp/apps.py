4659e8f16ba9b9602d297f83f0e0ca24258bfef2
Rubén Pallín González <RGonzalez@QUEIRUA.LOCAL>
2024-04-23 12:25:25 +0200

    Reestructuración del backend

--- maxrain-backend/maxrainapp/apps.py
+++ maxrain-backend/maxrainapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MaxrainapiConfig(AppConfig):
+class MaxrainappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'MaxrainApi'
+    name = 'maxrainapp'
